960484bc52e1ea6e20f9f5abd343beb4d5ae31f1
kazuyukitanimura <kazuyukitanimura@gmail.com>
2011-07-30 15:00:17 +0000

    4 cars

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ var io = socketIO.listen(app);
 //  osc_serv.bind(60000, '10.22.35.95')
 //
 //});
-MAXRACEUSER = 2;
+MAXRACEUSER = 4;
 gUserIDCounter = 0;
 gRaceIDCounter = 0;
 gRaceArray = [[]]; // array of array of sid
